82e63f98fa7932e04d73e85e6b1610b7939819b5
Tiri <thierrylaval@gmx.com>
2020-10-22 19:40:41 +0200

    Update user.js

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,9 +1,16 @@
+// Formation OpenClassrooms - Développeur Web - Projet 7 - Thierry Laval
+
+// Contient les fonctions qui s'appliquent aux différentes routes pour les utilisateurs
+
 // MODULES
+// On a besoin d'Express
 const express = require("express");
+// On crée un router avec la méthode mise à disposition par Express
 const router = express.Router();
 // FIN MODULES
 
 // IMPORTATION USER CONTROLLERS
+// On associe les fonctions aux différentes routes, on importe le controller
 const userCtrl = require("../controllers/user");
 // FIN IMPORTATION
 
