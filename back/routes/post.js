5e1728425b8b3a2c0ab2c4a03e2951b4561b652a
Tiri <thierrylaval@gmx.com>
2020-10-22 19:40:39 +0200

    Update post.js

--- back/routes/post.js
+++ back/routes/post.js
@@ -1,5 +1,9 @@
+// Formation OpenClassrooms - Développeur Web - Projet 7 - Thierry Laval
+
 // MODULES
+// Ajout de plugin externe nécessaire pour utiliser le router d'Express
 const express = require("express");
+// Appel du routeur avec la méthode mise à disposition par Express
 const router = express.Router();
 // FIN MODULES
 
@@ -8,11 +12,19 @@ const postCtrl = require("../controllers/post");
 // FIN IMPORTATION
 
 // IMPORTATION MIDDLEWARES
+// On importe le middleware auth pour sécuriser les routes
 const auth = require("../middleware/auth"); // Crée un token d'identification
+//On importe le middleware multer pour la gestion des images
 const multer = require("../middleware/multer-config"); // Permet d'envoyer un fichier dans la requête
 // FIN IMPORTATION
 
+
+// En exportant dans le controller la logique métier, les fonctions, on voit plus clairement quelles sont les routes dont on dispose
+// et on utilisera une sémantique très claire pour comprendre ce qu'elles permettent.
+// On a quelque chose de plus modulaire plus facile à comprendre et plus facile à maintenir
+
 // ROUTES
+// Création des différentes ROUTES de l'API en leurs précisant, dans l'ordre, leurs middlewares et controllers
 router.get("/", auth, postCtrl.getAllPosts);
 router.get("/:id", auth, postCtrl.getOnePost);
 router.post("/", auth, multer, postCtrl.createPost);
