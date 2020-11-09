7656c52d9d52b79d9a90bc28ed946791e4f2cad3
Tiri <thierrylaval@gmx.com>
2020-11-09 11:02:35 +0100

    Update babel.config.js

--- front/babel.config.js
+++ front/babel.config.js
@@ -1,6 +1,5 @@
 // En ajoutant un fichier babel. config. js,
-// 
-Babel résoudra facilement la configuration au lieu de chercher dans chaque fichier
+// Babel résoudra facilement la configuration au lieu de chercher dans chaque fichier
 // jusqu'à ce qu'il trouve le code de configuration.
 // Mais cela permettrait également de tirer parti d'une nouvelle option de configuration, overrides
 // Dans ce cas ajout de vue.
